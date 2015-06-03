ef2ad698db17ea1b791f54ddb715d6b08bb0f343
qixingyue <xingyue@simplemac.local>
2015-06-03 17:37:21 +0800

    添加嵌入式安装mysql

--- install/install_php.sh
+++ install/install_php.sh
@@ -28,3 +28,32 @@
 
 #if server is apache add this line after Type module
 #AddType application/x-httpd-php .php .php3 .phtml .inc
+
+
+
+#嵌入式安装mysql
+ # './configure' \
+ # '--prefix=/home/sinanet/run/php' \
+ # '--disable-short-tags' \
+ # '--with-curl' \
+ # '--enable-mysqlnd' \
+ # '--with-pdo-mysql' \
+ # '--with-config-file-path=/home/sinanet/run/php/' \
+ # '--sysconfdir=/home/sinanet/run/php/etc/' \
+ # '--with-pcre-dir' \
+ # '--enable-bcmath' \
+ # '--enable-zip' \
+ # '--enable-pcntl' \
+ # '--enable-redis' \
+ # '--enable-memcache' \
+ # '--enable-memcached' \
+ # '--enable-mbstring' \
+ # '--enable-inifile' \
+ # '--enable-fpm' \
+ # '--enable-embedded-mysqli' \
+ # "$@"
+ # 
+ # 
+ # #'--with-mysql=/data0/mysql5' \
+ # #'--with-pdo-mysql=/data0/mysql5' \
+ # #'--with-mysqli=/data0/mysql5/bin/mysql_config' \
