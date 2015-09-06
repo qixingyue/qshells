e27d735986e5424d81dc1bb7f282802a5c8a1c8c
xingyue <qixingyue@126.com>
2015-09-06 14:32:45 +0800

    添加安装mysql5.7脚本

---
+++ install/install_mysql_57.sh
@@ -0,0 +1,34 @@
+#! /bin/sh
+
+#useradd mysql -r -d /dev/null -s /sbinlogin 
+
+MYSQL_PREFIX=/data0/mysql
+
+#cmake \
+	#-DDOWNLOAD_BOOST=1 \
+	#-DWITH_BOOST=/data0/boost1.7 \
+	#-DCMAKE_INSTALL_PREFIX=$MYSQL_PREFIX \
+	#-DDEFAULT_CHARSET=utf8 \
+	#-DDEFAULT_COLLATION=utf8_general_ci \
+	#-DWITH_EXTRA_CHARSETS=all \
+	#-DWITH_MYISAM_STORAGE_ENGINE=1 \
+	#-DWITH_INNOBASE_STORAGE_ENGINE=1 \
+	#-DWITH_ZLIB=bundled \
+	#-DWITH_READLINE=1 \
+	#-DWITH_DEBUG=OFF \
+	#-DENABLED_LOCAL_INFILE=1 \
+	#-DENABLED_PROFILING=1 \
+	#-DMYSQL_MAINTAINER_MODE=0 \
+	#-DMYSQL_TCP_PORT=3306 \
+	#-DMYSQL_DATADIR=$MYSQL_PREFIX/data \
+	#-DMYSQL_UNIX_ADDR=$MYSQL_PREFIX/data/mysql.sock 
+#
+#make 
+#make install
+#
+#chmod u+x ./scripts/mysql_install_db.sh
+#mkdir -p $MYSQL_PREFIX/logs
+./scripts/mysql_install_db.sh --basedir="$MYSQL_PREFIX" --datadir="$MYSQL_PREFIX/data" --user=mysql
+# cp support-files/my-medium.cnf "$MYSQL_PREFIX/my.cnf"
+cp my.cnf "$MYSQL_PREFIX/my.cnf"
+
