9ffa0223d5a08958a9037e0ba40688b36441f487
xingyue <qixingyue@126.com>
2017-03-23 13:52:16 +0800

    sed replace home dir

--- install_zsh.sh
+++ install_zsh.sh
@@ -4,6 +4,8 @@
 yum install zsh -y 
 chsh -s /bin/zsh 
 yum install git -y 
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+git clone git://github.com/robbyrussell/oh-my-zsh.git /usr/local/oh-my-zsh
 
+cp /usr/local/oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+
+sed -i "s/export ZSH=\$HOME\/.oh-my-zsh/\/usr\/local\/oh-my-zsh/" .zshrc 
