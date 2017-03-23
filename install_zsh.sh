0418eaefe19d046c2f6d34c775a9b0f4810f15ba
xingyue <qixingyue@126.com>
2017-03-23 12:27:18 +0800

    make sure you have git command

--- install_zsh.sh
+++ install_zsh.sh
@@ -3,6 +3,7 @@
 
 yum install zsh -y 
 chsh -s /bin/zsh 
+yum install git -y 
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 
