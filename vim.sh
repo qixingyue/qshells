48c148e477d98fb76f43e6a27188ad98c1ffd804
xingyue <qixingyue@126.com>
2018-04-07 12:53:25 +0800

    vim shells

--- vim.sh
+++ vim.sh
@@ -2,4 +2,10 @@
 
 touch ~/.vimrc
 mv ~/.vimrc ~/.vimrc.bk
-curl -O https://
+curl -O https://raw.githubusercontent.com/qixingyue/qshells/master/vimrc
+mv vimrc ~/.vimrc
+
+echo "==========================="
+vim +BundleClean +BundleInstall! +qa
+chown $USER ~/.vim/
+echo 'down! enjoy it!'
